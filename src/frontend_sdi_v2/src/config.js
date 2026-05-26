c32a7109391e366af332beabe843ce8d609f22e6
Andrei-Ilie Mateian <mailandrei2014@gmail.com>
2026-05-26 17:01:59 +0300

    howtf

--- src/frontend_sdi_v2/src/config.js
+++ src/frontend_sdi_v2/src/config.js
@@ -1,3 +1,2 @@
-export const BASE_URL = import.meta.env. || 'https://localhost:8000';
-VITE_API_URL
+export const BASE_URL = import.meta.env.VITE_API_URL || 'https://localhost:8000';
 export const WB_URL = `${BASE_URL.replace(/^http/, 'ws')}`
