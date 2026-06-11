0066f04f9e8cd5a4f36038c6e6bb1c0dd38c969e
Andrei-Ilie Mateian <mailandrei2014@gmail.com>
2026-06-11 20:02:37 +0300

    qwq

--- src/frontend_sdi_v2/src/config.js
+++ src/frontend_sdi_v2/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://localhost:8000';
+export const BASE_URL = import.meta.env.VITE_API_URL ||  'https://localhost:8000';
 export const WB_URL = `${BASE_URL.replace(/^http/, 'ws')}`
